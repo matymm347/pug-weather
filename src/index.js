93d738b67ff77f3c99df92c441dada782f2e3f51
matymm347 <mateuszjochimiak@gmail.com>
2024-03-06 08:49:33 +0100

    Main page idea created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
+import "./style.css";
+
 let container = document.querySelector("#container");
 let heading = document.createElement("h1");
-heading.textContent = "Heading";
+heading.textContent = "Pug Weather";
 container.appendChild(heading);
+
+let searchBar = document.createElement("div");
+searchBar.id = "search-bar";
+container.appendChild(searchBar);
+
+let searchbutton = document.createElement("button");
+searchbutton.className = "gg-search";
+searchbutton.style.marginLeft = "18px";
+searchBar.appendChild(searchbutton);
+
+let apiUrl =
+  "http://api.weatherapi.com/v1/current.json?key=a5e4fed568014feea07121139242401&q=wroclaw&aqi=no";
+
+fetch(apiUrl, { mode: "cors" })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    console.log(`Current temperature: ${response.current.temp_c}`);
+  })
+  .catch(function (err) {
+    // Error :(
+  });
