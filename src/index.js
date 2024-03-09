9c5f3c8747b7fc181a4b3bfd3dfca4448fde74f9
matymm347 <mateuszjochimiak@gmail.com>
2024-03-09 18:22:27 +0100

    City autofill functionality added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import "./style.css";
 import searchIconImg from "./graphics/search-alt-1-svgrepo-com.svg";
 
+async function fillSearchHints(searchArea, userInput) {
+  let apiSearchURLBase = `http://api.weatherapi.com/v1/search.json?key=***REMOVED***&q=${userInput}`;
+
+  const cityList = await fetch(apiSearchURLBase, { mode: "cors" }).then(
+    (response) => response.json()
+  );
+
+  for (let index = 0; index < cityList.length; index++) {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = `${cityList[index].name}, ${cityList[index].country}`;
+    searchArea.appendChild(paragraph);
+  }
+}
+
 function getCurrentTemerature(location) {
   let apiUrl = `http://api.weatherapi.com/v1/current.json?key=***REMOVED***&q=${location}&aqi=no`;
 
@@ -56,13 +70,38 @@ let textElements = [
   "This is the third text element",
 ];
 
-for (let index = 0; index < textElements.length; index++) {
-  let paragraph = document.createElement("p");
-  paragraph.textContent = textElements[index];
-  searchResultSection.appendChild(paragraph);
-}
-
 searchBar.addEventListener("click", () => {
   searchInputArea.textContent = "siema";
   searchInputArea.focus();
+});
+
+let timeoutSwitch = true;
+let timeoutId = "";
+
+searchInputArea.addEventListener("keydown", (event) => {
+  if (event.ctrlKey === true) {
+    return;
+  }
+
+  if (event.key === "Enter") {
+    // Submit search result
+  }
+
+  if (timeoutSwitch === false) {
+    clearTimeout(timeoutId);
+  }
+
+  timeoutSwitch = false;
+
+  timeoutId = setTimeout(() => {
+    timeoutSwitch = true;
+    while (searchResultSection.firstChild) {
+      searchResultSection.removeChild(searchResultSection.firstChild);
+    }
+    if (searchInputArea.value != "") {
+      //API request
+      fillSearchHints(searchResultSection, searchInputArea.value);
+      //
+    }
+  }, 500);
 });
