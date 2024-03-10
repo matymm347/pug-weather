464d46cf4c38edbbb42b6c72e1e1a3abd713cc49
matymm347 <mateuszjochimiak@gmail.com>
2024-03-10 18:29:55 +0100

    Added back button without functionality, Added day marker, needs polishing

--- src/weatherPage.js
+++ src/weatherPage.js
@@ -1,4 +1,5 @@
 import { createHomeTitle } from "./homePage";
+import backIconSvg from "./graphics/back-svgrepo-com.svg";
 
 function getCurrentTemerature(locationId) {
   let apiUrl = `http://api.weatherapi.com/v1/current.json?key=a5e4fed568014feea07121139242401&q=id:${locationId}&aqi=no`;
@@ -15,19 +16,105 @@ function getCurrentTemerature(locationId) {
     });
 }
 
+function setupBackButton() {
+  let backButtonArea = document.createElement("div");
+  backButtonArea.id = "back-button-area";
+
+  let backButton = document.createElement("img");
+  backButton.src = backIconSvg;
+  backButton.id = "back-button";
+  backButton.alt = "back";
+  backButtonArea.appendChild(backButton);
+
+  return backButtonArea;
+}
+
+function setupDaySwitchMarker(currentDay) {
+  let switchContainer = document.createElement("div");
+  switchContainer.id = "switch-container";
+
+  let currentPositionElement = document.createElement("div");
+  currentPositionElement.id = "current-position-element";
+  currentPositionElement.className = "switch-element";
+  switchContainer.appendChild(currentPositionElement);
+
+  switch (currentDay) {
+    case 0:
+      currentPositionElement.style.order = 0;
+      break;
+    case 1:
+      currentPositionElement.style.order = 2;
+      break;
+    case 2:
+      currentPositionElement.style.order = 4;
+      break;
+    case 3:
+      currentPositionElement.style.order = 6;
+      break;
+  }
+
+  let backgroundPositionElement1 = document.createElement("div");
+  backgroundPositionElement1.style.order = "1";
+  backgroundPositionElement1.className =
+    "switch-background-element switch-element";
+  switchContainer.appendChild(backgroundPositionElement1);
+  let backgroundPositionElement2 = document.createElement("div");
+  backgroundPositionElement2.style.order = "3";
+  backgroundPositionElement2.className =
+    "switch-background-element switch-element";
+  switchContainer.appendChild(backgroundPositionElement2);
+  let backgroundPositionElement3 = document.createElement("div");
+  backgroundPositionElement3.style.order = "5";
+  backgroundPositionElement3.className =
+    "switch-background-element switch-element";
+  switchContainer.appendChild(backgroundPositionElement3);
+
+  return switchContainer;
+}
+
+function setupTopNowCardGrid(container) {
+  let topNowCardGrid = document.createElement("div");
+  topNowCardGrid.id = "top-now-card-grid";
+  topNowCardGrid.style.gridTemplateColumns = "1fr 1fr 1fr";
+  topNowCardGrid.style.direction = "row";
+
+  let backbutton = setupBackButton();
+  let switchMarker = setupDaySwitchMarker(3);
+  topNowCardGrid.appendChild(backbutton);
+  topNowCardGrid.appendChild(switchMarker);
+
+  container.appendChild(topNowCardGrid);
+}
+
+function setUpNowCard(container) {
+  let nowCard = document.createElement("div");
+  nowCard.style.gridTemplateRows = "1fr 1fr 1fr 5fr";
+  nowCard.id = "now-card";
+
+  setupTopNowCardGrid(nowCard);
+
+  container.appendChild(nowCard);
+}
+
+function setUpDetailCard(container) {
+  let detailCard = document.createElement("div");
+  detailCard.id = "detail-card";
+  detailCard.textContent = "detailCard";
+  container.appendChild(detailCard);
+}
+
 function setUpWeatherPage(locationId) {
   let container = document.querySelector("#container");
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 
-  createHomeTitle(container);
+  let infoGrid = document.createElement("div");
+  infoGrid.id = "info-grid";
+  container.appendChild(infoGrid);
 
-  //Testing
-  let testdiv = document.createElement("div");
-  testdiv.textContent = locationId;
-  container.appendChild(testdiv);
-  //
+  setUpNowCard(infoGrid);
+  setUpDetailCard(infoGrid);
 }
 
 export { setUpWeatherPage };
