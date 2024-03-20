7d0f30e39669821d9eb9b84049740dfa811e93f9
matymm347 <mateuszjochimiak@gmail.com>
2024-03-20 20:34:37 +0100

    Security update - API key now hidden with dotenv pack.

--- src/homePage.js
+++ src/homePage.js
@@ -3,7 +3,7 @@ import { setUpWeatherPage } from "./weatherPage";
 
 async function fillSearchHints(searchArea, userInput) {
   userInput = replacePolishChars(userInput);
-  let apiSearchURLBase = `http://api.weatherapi.com/v1/search.json?key=***REMOVED***&q=${userInput}&lang=pl`;
+  let apiSearchURLBase = `http://api.weatherapi.com/v1/search.json?key=${process.env.WEATHER_API_KEY}&q=${userInput}&lang=pl`;
 
   const cityList = await fetch(apiSearchURLBase, { mode: "cors" }).then(
     (response) => response.json()
