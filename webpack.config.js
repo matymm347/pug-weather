5ddbc7f413502f8f8ec4a0934c39714a051cde81
matymm347 <mateuszjochimiak@gmail.com>
2024-03-22 22:26:29 +0100

    Removed dotenv config and spare dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -8,7 +7,6 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new Dotenv()],
   module: {
     rules: [
       {
